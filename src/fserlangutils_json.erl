e2114d94d348be94071c60b7ab30a0c78650ec37
Farruco Sanjurjo <madtrick@gmail.com>
2012-11-20 21:05:24 +0100

    Module for json encoding/decoding
     * Includes jiffy as dep

---
+++ src/fserlangutils_json.erl
@@ -0,0 +1,42 @@
+-module(fserlangutils_json).
+
+-export([encode/1, decode/1]).
+-export([to_json_structure/1, tuple_to_json_structure/1, proplist_to_json_structure/1]).
+
+%%%%%%%%%%%%%%%%%%%%
+% API
+%%%%%%%%%%%%%%%%%%%%
+-spec decode(Input::string()) -> term().
+decode(Input) ->
+  try jiffy:decode(Input) of
+    Value -> Value
+    catch
+      _:{error,{_, truncated_json}} -> {error, empty_input};
+      _:{error,{_, E}} -> {error, {invalid_json, E}}
+    end.
+
+
+-spec encode(Input::term()) -> string().
+encode(Value) ->
+  jiffy:encode(Value).
+
+
+to_json_structure(Data) when is_list(Data) ->
+  case lists:all(fun({_,_}) -> true ; (_) -> false end, Data) of
+    true -> proplist_to_json_structure(Data);
+    false -> Data
+  end;
+to_json_structure(Data) when is_tuple(Data)->
+  tuple_to_json_structure(Data);
+to_json_structure(Data) ->
+  Data.
+
+tuple_to_json_structure({Field, Value}) ->
+  {[{Field, to_json_structure(Value)}]}.
+
+proplist_to_json_structure(Proplist) ->
+  {
+    lists:foldr(fun({Key, Value}, Acc) ->
+          [{Key, to_json_structure(Value)} | Acc]
+      end, [], Proplist )
+  }.
