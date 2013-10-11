c5a0bbaf2b1d725eb32733855411612303a7c035
Farruco Sanjurjo <madtrick@gmail.com>
2013-10-11 18:05:37 +0200

    Add a function to get microseconds since epoch

--- src/fserlangutils_time.erl
+++ src/fserlangutils_time.erl
@@ -1,7 +1,17 @@
 -module(fserlangutils_time).
 
--export([seconds_since_epoch/0]).
+-export([seconds_since_epoch/0, microseconds_since_epoch/0]).
 
 seconds_since_epoch() ->
-  {Megasecs, Secs, _Microsecs} = now(),
-  (Megasecs* 1000) + Secs .
+  {Megasecs, Secs, _Microsecs} = erlang:now(),
+  megaseconds_to_seconds(Megasecs) + Secs.
+
+hmicroseconds_since_epoch() ->
+  {Megasecs, Secs, Microsecs}  = erlang:now(),
+  megaseconds_to_seconds(Megasecs) + seconds_to_microseconds(Secs) + Microsecs.
+
+megaseconds_to_seconds(Megasecs) ->
+  Megasecs * 1000000.
+
+seconds_to_microseconds(Seconds) ->
+  Seconds * 1000000.
