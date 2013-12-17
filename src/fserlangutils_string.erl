901e68922162770060b179c175efdb6b96286a17
Farruco Sanjurjo <madtrick@gmail.com>
2013-12-17 18:22:52 +0100

    Add a to_integer function to string module

--- src/fserlangutils_string.erl
+++ src/fserlangutils_string.erl
@@ -1,6 +1,6 @@
 -module(fserlangutils_string).
 
--export([ensure_binary/1, ensure_list/1]).
+-export([ensure_binary/1, ensure_list/1, to_integer/1]).
 
 -spec ensure_binary(Data :: list()) -> binary();
                   (Data:: atom())-> binary();
@@ -15,3 +15,8 @@ ensure_binary(Data) when is_atom(Data) -> atom_to_binary(Data, utf8).
 ensure_list(Data) when is_list(Data) -> Data;
 ensure_list(Data) when is_atom(Data) -> atom_to_list(Data);
 ensure_list(Data) when is_binary(Data) -> binary_to_list(Data).
+
+-spec to_integer(String :: list()) -> integer().
+to_integer(String) ->
+  {Integer, _} = string:to_integer(String),
+  Integer.
